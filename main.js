f65456e7f4761617872465be4b8f13cc785769b5
luis coelho <luiscoelho18994@gmail.com>
2022-07-17 21:32:35 +0100

    create player and moves

---
+++ main.js
@@ -0,0 +1,13 @@
+let persona1 = document.querySelector("#persona1")
+let argola1 = document.querySelector("argola1")
+let argola2 = document.querySelector("argola2")
+let argola3 = document.querySelector("argola3")
+let argola4 = document.querySelector("argola4")
+
+
+
+function controle(){
+    if(persona1.classList != "animation"){
+        persona1.classList.add("animation")
+    }
+} 
